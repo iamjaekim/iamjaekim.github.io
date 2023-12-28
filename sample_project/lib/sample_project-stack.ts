ff0b2e58ee1bed083f22580d6366ed39adc0c862
Jae Wook Kim <jipod30@hotmail.com>
2023-12-28 16:43:10 -0500

    article wrapup

--- sample_project/lib/sample_project-stack.ts
+++ sample_project/lib/sample_project-stack.ts
@@ -1,17 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, CfnOutputProps, Stack, StackProps, aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class SampleProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class SampleProjectStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * Define vpc properties with ec2.VpcProps type to
+     * safe guard the data passing to VPC construct
+     */
+    const vpc_properties: ec2.VpcProps = {
+      vpcName: 'Sample-Project-DefaultVPC'
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SampleProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /**
+     * Creating New VPC with defined properties
+     */
+    const sample_project_vpc = new ec2.Vpc(this, 'sampleprojectvpc', vpc_properties)
 
+    /**
+     * Define cloudfomration export property object 
+     */
+    const export_vpc_id: CfnOutputProps = {
+      exportName: 'sample-project-vpc-id',
+      value: sample_project_vpc.vpcId
+    }
+    /**
+     * Create Cloudformation output 
+     */
+    new CfnOutput(this, 'export-vpc-id', export_vpc_id)
   }
 }
